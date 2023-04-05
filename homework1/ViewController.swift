4391b2164f16c0ef74d71c0b7462b0f63cd754eb
Ilya Klyukovskiy <klyukovskiy@yandex.ru>
2023-04-05 16:43:30 +0300

    first

--- homework1/ViewController.swift
+++ homework1/ViewController.swift
@@ -8,11 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var trafficIndex = 0
 
+    @IBOutlet var startBtn: UIButton!
+    @IBOutlet var trafficBtns: [UIView]!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        if let trafficBtns = trafficBtns {
+            for item in trafficBtns {
+                item.alpha = 0.5
+                item.layer.cornerRadius = 50
+            }
+        }
     }
+    
+    @IBAction func startBtnHandler(_ sender: UIButton) {
+        if let trafficBtns = trafficBtns {
+            startBtn.setTitle("Next", for: .normal)
+            if trafficIndex <= trafficBtns.count - 1 {
+                trafficBtns[trafficIndex].alpha = 1
+                trafficIndex += 1
+            } else {
+                startBtn.setTitle("Start", for: .normal)
+                trafficIndex = 0
+                for btn in trafficBtns {
+                    btn.alpha = 0.5
+                }
+                return
+            }
+        }
+    }
+    
 
 
 }
